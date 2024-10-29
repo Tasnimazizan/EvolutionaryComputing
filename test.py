e5dde2148b1fdc251a6a7af273dfa9efe27ecb70
Nur Tasnim Azizan <94262428+Tasnimazizan@users.noreply.github.com>
2024-10-29 13:37:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ st.header("Genetic Algorithm")
 POP_SIZE = 500
 
 #TARGET: Our goal.
-TARGET = st.text_input("Enter Your Name")
+TARGET = st.text_input("Enter Your Name","tasnim")
 
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
