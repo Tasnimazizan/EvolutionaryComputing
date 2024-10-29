025eb473559010fd8ac728e1b9dda6e09b7d8097
Nur Tasnim Azizan <94262428+Tasnimazizan@users.noreply.github.com>
2024-10-29 13:30:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ st.header("Genetic Algorithm")
 #POP_SIZE: Number of Chromosomes in our list.
 POP_SIZE = 500
 
+#TARGET: Our goal.
+TARGET = st.text_input("Enter Your Name")
+
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
 MUT_RATE = st.number_input("Enter Your Mutation Rate")
-
-#TARGET: Our goal.
-TARGET = st.text_input("Enter Your Name")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
