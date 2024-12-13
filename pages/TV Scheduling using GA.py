31de801ee1e055f9c5f54418a071695d5e368f6f
Nur Tasnim Azizan <94262428+Tasnimazizan@users.noreply.github.com>
2024-12-14 06:07:08 +0800

    Update TV Scheduling using GA.py

--- pages/TV Scheduling using GA.py
+++ pages/TV Scheduling using GA.py
@@ -1,4 +1,5 @@
 import csv
+import streamlit as st
 
 # Function to read the CSV file and convert it to the desired format
 def read_csv_to_dict(file_path):
@@ -149,4 +150,4 @@ print("\nFinal Optimal Schedule:")
 for time_slot, program in enumerate(final_schedule):
     print(f"Time Slot {all_time_slots[time_slot]:02d}:00 - Program {program}")
 
-print("Total Ratings:", fitness_function(final_schedule))
+st.write("Total Ratings:", fitness_function(final_schedule))
